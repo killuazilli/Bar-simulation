dc657ad0d7834eeeb9d9c6dc2398b590b77bfae2
killuazilli <stephaniemensah036@gmail.com>
2026-08-23 08:27:07 +0100

    tested dialogue

---
+++ BAR SIM/Assets/Scripts/DialogueTest.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class DialogueTest : MonoBehaviour
+{
+    [SerializeField] private DialogueManager dialogueManager;
+
+    public void ShowFirstChoices()
+    {
+        List<DialogueChoice> choices = new List<DialogueChoice>();
+
+        DialogueChoice supportive = new DialogueChoice
+        {
+            choiceText = "You seem upset. Do you want to talk about it?",
+            npcResponse = "Thanks... I think I actually do need someone to talk to.",
+            moodEffect = 2
+        };
+
+        DialogueChoice neutral = new DialogueChoice
+        {
+            choiceText = "What would you like to drink?",
+            npcResponse = "I don't know. Anything, really.",
+            moodEffect = 0
+        };
+
+        DialogueChoice dismissive = new DialogueChoice
+        {
+            choiceText = "Everyone has bad days.",
+            npcResponse = "Yeah... never mind.",
+            moodEffect = -1
+        };
+
+        choices.Add(supportive);
+        choices.Add(neutral);
+        choices.Add(dismissive);
+
+        dialogueManager.DisplayChoices(choices);
+    }
+}
