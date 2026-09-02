a047544448ac6fee3a4cc57bcf3d46aff6494c6c
killuazilli <stephaniemensah036@gmail.com>
2026-09-02 10:57:17 +0100

    sadcustomer,drinkchoices,ui setup

---
+++ BAR SIM/Assets/Scripts/DrinkChoice.cs
@@ -0,0 +1,9 @@
+using System;
+
+[Serializable]
+public class DrinkChoice
+{
+    public string actionName;
+    public int moodEffect;
+    public string npcResponse;
+}
--- BAR SIM/Assets/Scripts/DrinkSystem.cs
+++ BAR SIM/Assets/Scripts/DrinkSystem.cs
@@ -1,9 +1,39 @@
-using System;
+using UnityEngine;
+using TMPro;
 
-[Serializable]
-public class DrinkSystem
+public class DrinksSystem : MonoBehaviour
 {
-    public string actionName;
-    public int moodEffect;
-    public string npcResponse;
+    [Header("References")]
+    [SerializeField] private MoodSystem moodSystem;
+    [SerializeField] private TMP_Text dialogueText;
+
+    public void OfferWater()
+    {
+        moodSystem.ChangeMood(1);
+
+        dialogueText.text =
+            "Thanks. Water actually sounds good right now.";
+    }
+
+    public void OfferSoftDrink()
+    {
+        dialogueText.text =
+            "I'll have something light, thanks.";
+    }
+
+    public void OfferAlcoholicDrink()
+    {
+        moodSystem.ChangeMood(-1);
+
+        dialogueText.text =
+            "I'm not sure that's what I need right now.";
+    }
+
+    public void ContinueListening()
+    {
+        moodSystem.ChangeMood(1);
+
+        dialogueText.text =
+            "Thanks for listening. I needed that.";
+    }
 }
